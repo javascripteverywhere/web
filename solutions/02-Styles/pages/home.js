4f1e3ce2f96f4b26f2fc3d8ef80b3633c84ce773
Adam Scott <ascott1@gmail.com>
2019-06-28 07:08:56 -0400

    add button component

--- solutions/02-Styles/pages/home.js
+++ solutions/02-Styles/pages/home.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import Button from '../components/Button';
+
 const Home = () => {
   return (
     <div>
       <p>This is the home page</p>
+      <Button>Click me!</Button>
     </div>
   );
 };
