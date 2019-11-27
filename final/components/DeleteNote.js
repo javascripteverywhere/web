2585d5372961f4c8369bc4a09de1196d22c0c5bf
Adam Scott <ascott1@gmail.com>
2019-11-27 07:16:15 -0500

    route after note deletion

--- final/components/DeleteNote.js
+++ final/components/DeleteNote.js
@@ -4,19 +4,19 @@ import { withRouter } from 'react-router-dom';
 
 import ButtonAsLink from './ButtonAsLink';
 import { DELETE_NOTE } from '../gql/mutation';
-import { GET_MY_NOTES, GET_NOTES, GET_MY_FAVORITES } from '../gql/query';
+import { GET_MY_NOTES, GET_NOTES } from '../gql/query';
 
 const DeleteNote = props => {
   const [deleteNote] = useMutation(DELETE_NOTE, {
     variables: {
       id: props.noteId
     },
-    // refetch all of the note list queries to update the cache
-    refetchQueries: [
-      { query: GET_MY_NOTES },
-      { query: GET_NOTES },
-      { query: GET_MY_FAVORITES }
-    ]
+    // refetch the note list queries to update the cache
+    refetchQueries: [{ query: GET_MY_NOTES, GET_NOTES }],
+    onCompleted: data => {
+      // redirect the user to the "my notes" page
+      props.history.push('/mynotes');
+    }
   });
 
   return <ButtonAsLink onClick={deleteNote}>Delete Note</ButtonAsLink>;
